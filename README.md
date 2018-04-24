93bf37aa858dfed618e2d8c2aaa507c680b7d583
m5211129 <s.ikeda107@gmail.com>
2018-04-24 18:01:15 +0900

    upgrade MersenneTwister from 2002 to 2004 version

--- README.md
+++ README.md
@@ -10,8 +10,9 @@ int型1と0のxor演算子の結果を確認するプログラム
 ## test_copyStructure.c
 構造体のコピーができるか確認するプログラム  
 ## test_mt.c
-乱数生成装置メルセンヌツイスタの実行確認プログラム
+乱数生成装置メルセンヌツイスタの実行確認プログラム  
+[64bit版 Mersenne Twister](http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/mt64.html)
 ## test_fileIO.c
-複数ファイルを出力する方法を確認するプログラム 
+複数ファイルを出力する方法を確認するプログラム
 ## sample_matplotlib.py
 python3 Matplotlibによるグラフ描画サンプルプログラム
--- test_mt.c
+++ test_mt.c
@@ -14,24 +14,19 @@
 // }
 #include <stdio.h>
 #include <time.h>
-#include "./MersenneTwister/mt19937ar.h"
+#include "./MersenneTwister/mt19937-64.h"
 
 int main(void){
   int i;
-  init_genrand((unsigned)time(NULL));//初期化
+  init_genrand64((unsigned)time(NULL));//初期化
   for(i=0;i<10;i++){
-    printf("%ld\n",genrand_int32());//乱数生成
+    printf("%d\n",genrand64_int63());//乱数生成
   }
   for(i=0;i<100;i++){
-    printf("%ld ",genrand_int32()%10);//0~9の乱数生成
+    //genrand64_real1() generates a random number on [0,1]-real-interval
+    printf("%d ",(int)(genrand64_real1()*10));//random number on integral [0,9]
+    printf("%f ",genrand64_real1());//random number on [0,1]
     if(i%10==9)printf("\n");
   }
   return 0;
 }
-//乱数生成関数一覧
-// genrand_int32() //符号なし32ビット長整数
-// genrand_int31() //符号なし31ビット長整数
-// genrand_real1() //一様実乱数[0,1] (32ビット精度)
-// genrand_real2() //一様実乱数[0,1) (32ビット精度)
-// genrand_real3() //一様実乱数(0,1) (32ビット精度)
-// genrand_res53() //一様実乱数[0,1) (53ビット精度
