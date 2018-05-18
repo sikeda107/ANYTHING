cb0b1e34f6b43400f9335fe26d029cbfc7807c34
m5211129 <s.ikeda107@gmail.com>
2018-05-18 16:07:32 +0900

    add print log

--- test_macro.c
+++ test_macro.c
@@ -5,9 +5,13 @@
 // 引数に#をつけると文字列定数化できる
 #define GET_VALUE_NAME(n) #n
 
+//組み込みマクロを使ったログの表示
+#define PRINT_LOG( log ) printf( "File:%s Line:%d Func:%s Log:%s\n", __FILE__, __LINE__, __func__, log )
+
 int main(int argc, char const *argv[]) {
   int value = 0;
-
+  char logname[256] = "MYLOG";
   printf("%s\n",GET_VALUE_NAME(value));
+  PRINT_LOG(logname);
   return 0;
 }
